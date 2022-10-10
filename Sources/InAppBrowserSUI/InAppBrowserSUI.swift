8be72153e06bfaa33fac51b2e738c5f49689ba86
Kamaal <kamaal.f1@gmail.com>
2022-10-10 17:34:49 +0200

    Binding to url

--- Sources/InAppBrowserSUI/InAppBrowserSUI.swift
+++ Sources/InAppBrowserSUI/InAppBrowserSUI.swift
@@ -12,8 +12,8 @@ extension View {
     /// On iOS opens the given `URL` in a in app browser when `isPresented` state is set to true and on macOS opens
     /// your default browser with the given `URL`.
     /// - Parameters:
-    ///   - isPresented: Whether to open the in app browser or not.
-    ///   - url: The url to open in the in app browser.
+    ///   - isPresented: Whether to open the browser or not.
+    ///   - url: The url to open in the browser.
     ///   - color: The accent color on the browser buttons.
     /// - Returns: The modified view.
     ///
@@ -35,27 +35,78 @@ extension View {
     /// }
     /// ```
     public func inAppBrowserSUI(isPresented: Binding<Bool>, url: URL, color: Color = .accentColor) -> some View {
-        modifier(InAppBrowserSUI(isPresented: isPresented, url: url, color: color))
+        modifier(InAppBrowserSUI(
+            isPresented: isPresented,
+            url: .constant(url),
+            fallbackURL: url,
+            color: color,
+            bindingWithURL: false))
+    }
+
+    /// On iOS opens the given `URL` in a in app browser when `isPresented` state is set to true and on macOS opens
+    /// your default browser with the given `URL`.
+    /// - Parameters:
+    ///   - url: The url to show. If this url is not nil than the browser opens.
+    ///   - fallbackURL: A valid url in case the url given is invalid.
+    ///   - color: The accent color on the browser buttons.
+    /// - Returns: The modified view.
+    ///
+    /// Basic usage example:
+    ///
+    /// ```swift
+    /// import SwiftUI
+    /// import InAppBrowserSUI
+    ///
+    /// struct ContentView: View {
+    ///     @State private var urlToShowInBrowser: URL?
+    ///
+    ///     var body: some View {
+    ///         Button(action: { urlToShowInBrowser = URL(string: "https://kamaal.io") }) {
+    ///             Text("Open browser")
+    ///         }
+    ///         .inAppBrowserSUI($urlToShowInBrowser, fallbackURL: URL(string: "https://kamaal.io")!)
+    ///     }
+    /// }
+    /// ```
+    public func inAppBrowserSUI(_ url: Binding<URL?>, fallbackURL: URL, color: Color = .accentColor) -> some View {
+        return modifier(InAppBrowserSUI(
+            isPresented: .constant(url.wrappedValue != nil),
+            url: url,
+            fallbackURL: fallbackURL,
+            color: color,
+            bindingWithURL: true))
     }
 }
 
 fileprivate struct InAppBrowserSUI: ViewModifier {
     @Binding var isPresented: Bool
+    @Binding var url: URL?
 
-    let url: URL
+    let fallbackURL: URL
     let color: Color
+    let bindingWithURL: Bool
 
     func body(content: Content) -> some View {
         content
             #if canImport(UIKit)
-            .sheet(isPresented: $isPresented) {
-                InAppBrowserRepresentable(url: url, tintColor: color)
+            .sheet(isPresented: $isPresented, onDismiss: {
+                if bindingWithURL {
+                    url = nil
+                }
+            }) {
+                InAppBrowserRepresentable(url: url ?? fallbackURL, tintColor: color)
             }
             #else
             .onChange(of: isPresented, perform: { newValue in
-                if newValue {
+                if !bindingWithURL, newValue, let url {
                     NSWorkspace.shared.open(url)
                     isPresented = false
+                }
+            })
+            .onChange(of: url, perform: { newValue in
+                if bindingWithURL, let url = newValue {
+                    NSWorkspace.shared.open(url)
+                    self.url = nil
                 }
             })
             #endif
