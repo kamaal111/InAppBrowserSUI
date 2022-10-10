191c767f7226313002ea78903dab5f11e2e05d59
Kamaal <kamaal.f1@gmail.com>
2022-10-10 17:34:49 +0200

    Binding to url

--- Example/Example/ContentView.swift
+++ Example/Example/ContentView.swift
@@ -10,15 +10,23 @@ import InAppBrowserSUI
 
 struct ContentView: View {
     @State private var showInAppBrowser = false
+    @State private var urlToShowInBrowser: URL?
 
     var body: some View {
         VStack {
             Button(action: { showInAppBrowser = true }) {
                 Text("Open browser")
             }
+            Button(action: { urlToShowInBrowser = URL(string: "https://kamaal.io") }) {
+                Text("Open browser with url")
+            }
+            Button(action: { urlToShowInBrowser = URL(string: "https://kamaal.io/colorselector") }) {
+                Text("Open browser with other url")
+            }
         }
         .padding()
-        .inAppBrowserSUI(isPresented: $showInAppBrowser, url: URL(string: "https://kamaal.io")!, color: .red)
+        .inAppBrowserSUI(isPresented: $showInAppBrowser, url: URL(string: "https://kamaal.io")!, color: .yellow)
+        .inAppBrowserSUI($urlToShowInBrowser, fallbackURL: URL(string: "https://kamaal.io")!, color: .green)
         .accentColor(.red)
         #if os(macOS)
         .frame(minWidth: 300, minHeight: 300)
